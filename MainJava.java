82d78c67f5b9a394f35aa4508cdaef7c67f8b817
muradoworxan <muradov.orxan.azad@bsu.edu.az>
2023-04-15 17:53:27 +0400

    sec

--- MainJava.java
+++ MainJava.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
 
+import javax.swing.JTable.PrintMode;
+
 public class MainJava {
 
     public static void main(String[] args) {
     System.out.println(" salam");
+        for (String string : args) {
+            System.out.println("hellouu");
+        }
     }
 }
