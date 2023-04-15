171931f23d608074815c8e891c4aa14b4fd9d3c5
Farhad <ferhad096@gmail.com>
2023-04-15 17:56:25 +0400

    thi

--- MainJava.java
+++ MainJava.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
-import javax.swing.JTable.PrintMode;
 
 public class MainJava {
 
     public static void main(String[] args) {
     System.out.println(" salam");
         for (String string : args) {
+            System.out.println("hellouu Man");
+            
             System.out.println("hellouu");
         }
+        
     }
 }
